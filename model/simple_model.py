0d6b73305056c65c4bafe49081d2975eb578d6a1
Tsanh Sun <suncanhang@gmail.com>
2018-12-25 22:01:02 +0800

    a simple model

--- model/simple_model.py
+++ model/simple_model.py
@@ -1,8 +1,11 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+from sys import path
+path.append("../")
+from controller.simple_control import CtrlPID
 class SpringMass(object):
 	"""docstring for SpringMass"""
-	def __init__(self, sys_para, states,noise=None):
+	def __init__(self, sys_para, states,ts, noise=None):
 		super(SpringMass, self).__init__()
 		self.m = sys_para[0]
 		self.k = sys_para[1]
@@ -10,26 +13,28 @@ class SpringMass(object):
 		self.noise = None
 		self.x0 = states[0]
 		self.xdot0 = states[1]
-		self.kp = 3
-		self.kd = 4
+		self.ts = ts
+		self.kp = 4
+		self.kd = 25
+		self.err1 = 0
 	def dyn_equation(self, states, u):
 		#xddot + r/m * xdot + k/m * x= u/m
 		# f is the input
 		x = states[0]
 		xdot = states[1]
-		xddot = (u - self.r * xdot -k * x) / self.m
+		xddot = (u - self.r * xdot -self.k * x) / self.m
 		return xddot
 
-	def pos_vel(self, inti_states, xddot, ts):
+	def pos_vel(self, inti_states, xddot):
 		x0 = inti_states[0]
 		xdot0 = inti_states[1]
-		xdot = xdot0 + xddot * ts
-		x = x0 + xdot * ts
+		xdot = xdot0 + xddot * self.ts
+		x = x0 + xdot * self.ts
 		return x, xdot
 
-	def process(self, ts, duration, des):
+	def process(self, duration, des):
 
-		time_arr = np.arange(0,duration,ts)
+		time_arr = np.arange(0,duration,self.ts)
 		step_len = len(time_arr)
 		x_arr = np.zeros((step_len,1))
 		xdot_arr = np.zeros((step_len,1))
@@ -37,26 +42,25 @@ class SpringMass(object):
 		xdot0 = self.xdot0
 		inti_states = [x0, xdot0]
 		err = des - inti_states[0]
+		ctrl = CtrlPID([10,21,2])
 		for time in np.arange(0,step_len,1):
 			x_arr[time] = inti_states[0]
 			xdot_arr[time] = inti_states[1]
 
-			u = self.control_PID(0, 0)
+			#u = self.control_PID(err,inti_states)
+			u = ctrl.ctrl_pid(err,self.ts)
 			xddot = self.dyn_equation(inti_states,u)
-			pos, vel = self.pos_vel(inti_states, xddot, ts)
+			pos, vel = self.pos_vel(inti_states, xddot)
 		
 			inti_states = [pos, vel]
+			err = des - pos
 		return time_arr, x_arr, xdot_arr
-	def control_PID(self, err, err_dot):
-
-		u = self.kp * err + self.kd * err_dot
+	def control_PID(self, err, states):
+		err_dot = (err - self.err1)/self.ts
+		u = (self.k * states[0] + self.r * states[1]) + self.kp * err + self.kd * err_dot
+		self.err1 = err
 		return u
-
-
-		
-
-
-if __name__ == '__main__':
+def test():
 	ts = 0.01
 	duration = 10
 	states = [2,0]
@@ -64,11 +68,15 @@ if __name__ == '__main__':
 	r = 4
 	k = 8
 	sys_para = [mass, k, r]
-	des = 3
-	spm = SpringMass(sys_para, states)
-	t,x,y = spm.process(ts, duration, 3)
+	des = -8
+	spm = SpringMass(sys_para, states, ts)
+	t,x,y = spm.process(duration, des)
 	plt.plot(t,x)
 	#plt.ion()
 	plt.show()
 
+
+if __name__ == '__main__':
+
+	test()
 
